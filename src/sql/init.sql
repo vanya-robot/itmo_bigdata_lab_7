d6d2e200fc61b4333e156289b8c9f6163de8bc7f
vanya-robot <alexeenkoyaroslav@mail.ru>
2025-09-09 19:29:29 +0300

    commented init.sql

--- src/sql/init.sql
+++ src/sql/init.sql
@@ -1,4 +1,4 @@
--- Create source_data table with columns matching lab5 preprocessing
+-- source data --
 CREATE TABLE IF NOT EXISTS source_data (
     id SERIAL PRIMARY KEY,
     energy_100g DOUBLE PRECISION,
@@ -8,14 +8,14 @@ CREATE TABLE IF NOT EXISTS source_data (
     sugars_100g DOUBLE PRECISION
 );
 
--- Insert sample rows (some variety, including a negative to test imputation)
+-- samples --
 INSERT INTO source_data (energy_100g, fat_100g, carbohydrates_100g, proteins_100g, sugars_100g) VALUES
 (250.0, 10.0, 30.0, 5.0, 8.0),
 (300.0, 12.0, 40.0, 8.0, 12.0),
 (180.0, 5.0, 20.0, 3.0, 4.0),
 (-1.0, 0.0, 0.0, 0.0, 0.0); -- -1 used to simulate invalid value -> will become NULL
 
--- Create predictions table used by lab_6 pipeline
+-- predictions table --
 CREATE TABLE IF NOT EXISTS predictions (
     id BIGINT,
     prediction INTEGER,
